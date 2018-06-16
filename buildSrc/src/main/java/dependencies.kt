7bc6d3ea85784089b3181e1d514133bc8f404493
IVIanuu <IVIanuu@gmail.com>
2018-06-16 19:44:48 +0200

    Cleaned up the dependency file

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -10,9 +10,6 @@ object Versions {
 
     const val androidGradlePlugin = "3.1.3"
     const val autoCommon = "0.8"
-    const val autoService = "1.0-rc3"
-
-    const val epoxy = "2.12.0"
 
     const val kotlin = "1.2.41"
     const val kotlinPoet = "0.7.0"
@@ -26,10 +23,6 @@ object Deps {
     const val androidGradlePlugin = "com.android.tools.build:gradle:${Versions.androidGradlePlugin}"
 
     const val autoCommon = "com.google.auto:auto-common:${Versions.autoCommon}"
-    const val autoService = "com.google.auto.service:auto-service:${Versions.autoService}"
-
-    const val epoxy = "com.airbnb.android:epoxy:${Versions.epoxy}"
-    const val epoxyProcessor = "com.airbnb.android:epoxy-processor:${Versions.epoxy}"
 
     const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
     const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jre7:${Versions.kotlin}"
